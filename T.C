441a76afd5b7687d3f8b5f8c0ff83b5b7ea8e09d
tajminhalageri <ziyaullahalageri@gmail.com>
2024-11-30 02:07:13 -0800

    Add files via upload

---
+++ T.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	FILE *file;
+	char data[500];
+
+	printf("Enter data to write to the file\n");
+	gets(data);
+
+	file = fopen("output.txt","w");
+	if(file == NULL)
+	{
+		printf("error opening file!\n");
+		return 1;
+	}
+	fprintf(file,"%s",data);
+	fclose(file);
+
+	printf("Data return to file successfully\n");
+	return 0;
+}
